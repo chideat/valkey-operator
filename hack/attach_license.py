433ccf3917508d950c4563801d29c2a7fd0f70c8
Seer <kvcnow@gmail.com>
2025-03-31 10:50:34 +0800

    fix: fix bug of generate crd

--- hack/attach_license.py
+++ hack/attach_license.py
@@ -10,6 +10,7 @@ import re
 with open('./hack/boilerplate.go.txt') as f:
     AUTH_COMMENT = f.read()
 
+PATTERN = r"/\*.*?Licensed under the Apache License.*?\*/\n?"
 EXTENSION = ".go"
 
 for root, dirs, files in os.walk('.'):
@@ -18,11 +19,12 @@ for root, dirs, files in os.walk('.'):
             file_path = os.path.join(root, file)
             with open(file_path, 'r+') as f:
                 content = f.read()
-                index = content.index("package ")
-                if index == 0:
-                    content = AUTH_COMMENT + '\n' + content
+                if re.search(PATTERN, content, re.DOTALL):
+                    # Replace existing block comment
+                    content = re.sub(PATTERN, AUTH_COMMENT, content, flags=re.DOTALL)
                 else:
-                    content = AUTH_COMMENT + '\n' + content[index:]
+                    # Prepend new comment
+                    content = AUTH_COMMENT + '\n' + content
                 f.seek(0)
                 f.write(content)
                 f.truncate()
